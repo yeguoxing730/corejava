0bfba474a67f4e335c3343bcb000e54b3f4bf70a
yeguoxing <yeguoxing@autohome.com.cn>
2018-05-31 10:30:54 +0800

    jdk内置xml解析

---
+++ src/main/java/com/xml/DomDemo.java
@@ -0,0 +1,122 @@
+package com.xml;
+
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.transform.*;
+import javax.xml.transform.dom.DOMSource;
+import javax.xml.transform.stream.StreamResult;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+public class DomDemo {
+    private Document document;
+    private String fileName;
+
+    public void init() {
+        try {
+            DocumentBuilderFactory factory = DocumentBuilderFactory
+                    .newInstance();
+            DocumentBuilder builder = factory.newDocumentBuilder();
+            this.document = builder.newDocument();
+        } catch (ParserConfigurationException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+    public void createXml(String fileName) {
+        //创建一个新的文档对象
+        init();
+        //根节点
+        Element root = this.document.createElement("employees");
+        //将根节点加载到文档对象中
+        this.document.appendChild(root);
+
+        //创建子元素
+        Element employee = this.document.createElement("employee");
+        Element name = this.document.createElement("name");
+
+        //添加文本节点
+        name.appendChild(this.document.createTextNode("qiankunshe"));
+
+        employee.appendChild(name);
+        Element age = this.document.createElement("age");
+        age.appendChild(this.document.createTextNode("25"));
+        employee.appendChild(age);
+
+        //添加层级关系
+        root.appendChild(employee);
+        TransformerFactory tf = TransformerFactory.newInstance();
+        try {
+            Transformer transformer = tf.newTransformer();
+            DOMSource source = new DOMSource(document);
+
+            //添加xml 头信息
+            transformer.setOutputProperty(OutputKeys.ENCODING, "gb2312");
+            transformer.setOutputProperty(OutputKeys.INDENT, "yes");
+
+            //输出xml流信息包装类
+            PrintWriter pw = new PrintWriter(new FileOutputStream(fileName));
+            StreamResult result = new StreamResult(pw);
+
+            //将xml写到文件中
+            transformer.transform(source, result);
+            System.out.println("生成XML文件成功!");
+        } catch (TransformerConfigurationException e) {
+            System.out.println(e.getMessage());
+        } catch (IllegalArgumentException e) {
+            System.out.println(e.getMessage());
+        } catch (FileNotFoundException e) {
+            System.out.println(e.getMessage());
+        } catch (TransformerException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+    public void parserXml(String fileName) throws Exception {
+        try {
+            DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
+            DocumentBuilder db = dbf.newDocumentBuilder();
+
+            //将xml文件解析
+            Document document = db.parse(fileName);
+
+            //获得所有节点，递归遍历节点
+            NodeList employees = document.getChildNodes();
+            for (int i = 0; i < employees.getLength(); i++) {
+                //取得一个节点
+                Node employee = employees.item(i);
+
+
+                NodeList employeeInfo = employee.getChildNodes();
+                for (int j = 0; j < employeeInfo.getLength(); j++) {
+                    Node node = employeeInfo.item(j);
+                    NodeList employeeMeta = node.getChildNodes();
+                    for (int k = 0; k < employeeMeta.getLength(); k++) {
+                        System.out.println(employeeMeta.item(k).getNodeName()
+                                + ":" + employeeMeta.item(k).getTextContent());
+                    }
+                }
+            }
+            System.out.println("解析完毕");
+        } catch (FileNotFoundException e) {
+            System.out.println(e.getMessage());
+        } catch (ParserConfigurationException e) {
+            System.out.println(e.getMessage());
+        }         catch (IOException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+    public static void main(String[] args)throws Exception{
+        DomDemo domDemo = new DomDemo();
+        domDemo.createXml("domxml.xml");
+        domDemo.parserXml("domxml.xml");
+    }
+}
---
+++ src/main/java/com/xml/XPathExample.java
@@ -0,0 +1,40 @@
+package com.xml;
+
+import org.w3c.dom.Document;
+import org.w3c.dom.NodeList;
+import org.xml.sax.SAXException;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.xpath.XPath;
+import javax.xml.xpath.XPathConstants;
+import javax.xml.xpath.XPathExpressionException;
+import javax.xml.xpath.XPathFactory;
+import java.io.IOException;
+
+public class XPathExample {
+    public static void main(String[] args)
+            throws ParserConfigurationException, SAXException,
+            IOException, XPathExpressionException {
+
+        DocumentBuilderFactory domFactory = DocumentBuilderFactory.newInstance();
+        domFactory.setNamespaceAware(true); // never forget this!
+        DocumentBuilder builder = domFactory.newDocumentBuilder();
+        Document doc = builder.parse("books.xml");
+
+        XPathFactory factory = XPathFactory.newInstance();
+        XPath xpath = factory.newXPath();
+        //one way by compile
+  //      XPathExpression expr                = xpath.compile("//book[author='Neal Stephenson']/title/text()");
+//        Object result = expr.evaluate(doc, XPathConstants.NODESET);
+        //the other way
+        Object result = xpath.evaluate("//book[author='Neal Stephenson']/title/text()",doc,XPathConstants.NODESET);
+        NodeList nodes = (NodeList) result;
+        for (int i = 0; i < nodes.getLength(); i++) {
+            System.out.println(nodes.item(i).getNodeValue());
+        }
+
+    }
+
+}

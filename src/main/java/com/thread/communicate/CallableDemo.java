15a81478ed7438ff22d12216a2aa962e4d2a9599
yeguoxing730 <yeguoxing730@163.comm>
2018-06-03 16:25:06 +0800

    thread

--- src/main/java/com/thread/communicate/CallableDemo.java
+++ src/main/java/com/thread/communicate/CallableDemo.java
@@ -1,7 +1,39 @@
 package com.thread.communicate;
 
+import com.mysql.cj.mysqlx.protobuf.MysqlxDatatypes;
+
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.FutureTask;
+
 /**
  * 子线程完成某件任务后，把得到的结果回传给主线程
  */
 public class CallableDemo {
+    public static void main(MysqlxDatatypes.Scalar.String[] args){
+        Callable<Integer> callable = new Callable<Integer>() {
+            @Override
+            public Integer call() throws Exception {
+                System.out.println("Task starts");
+                Thread.sleep(1000);
+                int result = 0;
+                for (int i=0; i<=100; i++) {
+                    result += i;
+                }
+                System.out.println("Task finished and return result");
+                return result;
+            }
+        };
+        FutureTask<Integer> futureTask = new FutureTask<>(callable);
+        new Thread(futureTask).start();
+        try {
+            System.out.println("Before futureTask.get()");
+            System.out.println("Result:" + futureTask.get());
+            System.out.println("After futureTask.get()");
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } catch (ExecutionException e) {
+            e.printStackTrace();
+        }
+    }
 }
---
+++ src/main/java/com/thread/communicate/CallablePool.java
@@ -0,0 +1,42 @@
+package com.thread.communicate;
+
+import java.sql.Connection;
+import java.util.concurrent.Callable;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.FutureTask;
+
+/**
+ * Created by yeguo on 2018/5/20.
+ */
+public class CallablePool {
+    private ConcurrentHashMap<String,FutureTask<Connection>> connectionPool = new ConcurrentHashMap<String, FutureTask<Connection>>();
+
+    public Connection getConnection(String key) throws Exception{
+        FutureTask<Connection>connectionTask=connectionPool.get(key);
+        if(connectionTask!=null){
+            return connectionTask.get();
+        }
+        else{
+            Callable<Connection> callable = new Callable<Connection>(){
+                @Override
+                public Connection call() throws Exception {
+                    // TODO Auto-generated method stub
+                    return createConnection();
+                }
+            };
+            FutureTask<Connection>newTask = new FutureTask<Connection>(callable);
+            connectionTask = connectionPool.putIfAbsent(key, newTask);
+            if(connectionTask==null){
+                connectionTask = newTask;
+                connectionTask.run();
+            }
+            return connectionTask.get();
+        }
+    }
+
+    //创建Connection
+    private Connection createConnection(){
+        return null;
+    }
+
+}
